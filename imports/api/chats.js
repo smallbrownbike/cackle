b161d8ffd9fa6b5d49dd22b219578c6da4f65030
smallbrownbike <ekinseydev@gmail.com>
2017-12-02 13:59:09 -0800

    working db

--- imports/api/chats.js
+++ imports/api/chats.js
@@ -1,4 +1,32 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
+// import { check } from 'meteor/check';
 
 export const Chats = new Mongo.Collection('chats');
+
+if (Meteor.isServer) {
+	Meteor.publish('chats', function chatsPublication(roomName) {
+		const chats = Chats.find({room: roomName}).fetch();
+		if(chats.length === 0){
+				const initialMessage = {
+					room: roomName,
+					messages: [
+						{
+							username: 'Botman',
+							message: `Welcome to ${roomName}! Say something interesting.`,
+							date: Date.now()
+						}
+					]
+						
+				}
+				Chats.insert(initialMessage)
+			}
+		return Chats.find({room: roomName});
+	});
+}
+
+Meteor.methods({
+    'chats.update'(id, message) {
+        Chats.update({_id: id}, {$push: {messages: message}})
+    }
+});
--- imports/ui/Components/Chat.js
+++ imports/ui/Components/Chat.js
@@ -3,27 +3,12 @@ import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import faker from 'faker';
 import randomcolor from 'randomcolor';
+import { Meteor } from 'meteor/meteor';
 
 import { Chats } from '../../api/chats'
 
 const roomName = window.location.pathname.slice(1),
 			username = faker.internet.userName();
-
-const chats = Chats.find({room: roomName}).fetch();	
-let bgColor;
-if(chats.length === 0){
-	const initialMessage = {
-		username: 'Botman',
-		message: `Welcome to ${roomName}! Say something interesting.`,
-		date: Date.now()
-	}
-	Chats.insert({
-		room: roomName,
-		messages: [
-			initialMessage
-		]
-	})
-}
 
 class ViewBox extends React.Component{
 	componentDidUpdate(){
@@ -35,9 +20,9 @@ class ViewBox extends React.Component{
 	render(){
 		return(
 			<div className='viewBox'>
-				{this.props.chats[0].messages.map(chat => {
+				{this.props.chats[0].messages.map((chat, index) => {
 					return (
-						<div ref={ref => this.lastMessage = ref} className='message'>
+						<div key={index} ref={ref => this.lastMessage = ref} className='message'>
 							<div className='username'>{chat.username}</div>
 							<div className='date'>{new Date(chat.date).toLocaleString()}</div>							
 							<div className='text'>{chat.message}</div>
@@ -60,12 +45,13 @@ class Chat extends React.Component{
 	}
 	handleKeyPress = e => {
 		if(e.key === 'Enter'){
-			message = {
-				username: username,
-				message: this.state.value,
-				date: Date.now()
-			}
-			Chats.update({_id: this.props.chats[0]._id}, {$push: {messages: message}})
+			const id = this.props.chats[0]._id,
+						message = {
+							username: username,
+							message: this.state.value,
+							date: Date.now()
+						};
+			Meteor.call('chats.update', id, message)
 			this.setState({value: ''})
 		}
 	}
@@ -74,7 +60,8 @@ class Chat extends React.Component{
 			<div className='container'>
 				<div className='online'></div>
 				<div className='chatBox'>
-					<ViewBox chats={this.props.chats}/>
+					{this.props.chats[0] && <ViewBox chats={this.props.chats}/>}
+					
 					<div className='inputBox'>
 						<input placeholder='Message' className='chatInput' value={this.state.value} onKeyPress={this.handleKeyPress} onChange={this.handleChange} />
 					</div>
@@ -85,11 +72,7 @@ class Chat extends React.Component{
 }
 
 export default withTracker(() => {
-	// 	bgColor = randomcolor.randomColor({luminosity: 'light'});
-	// } else {
-	// 	bgColor = chats.roomColor;
-	// }
-	// document.body.style.backgroundColor = bgColor;
+	Meteor.subscribe('chats', roomName);
 	return {
 	  chats: Chats.find({room: roomName}, {sort: {messages: {date: -1}}}).fetch(),
 	};
