4ec734cb53bbdbc50c4da03d2044a7ad15f59208
smallbrownbike <ekinseydev@gmail.com>
2017-12-02 02:11:48 -0800

    more styles

--- imports/ui/Components/Chat.js
+++ imports/ui/Components/Chat.js
@@ -4,26 +4,56 @@ export default class Chat extends React.Component{
 	render(){
 		return (
 			<div className='container'>
+				<div className='online'></div>
+				<div className='chatBox'>
 				<div className='viewBox'>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
-					<div className='message'>Hello world!</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
+					<div className='message'>
+						<div className='username'>Username</div>
+						<div className='text'>Text</div>
+					</div>
 				</div>
 				<input className='chatInput' />
+				</div>
 			</div>
 		)
 	}
