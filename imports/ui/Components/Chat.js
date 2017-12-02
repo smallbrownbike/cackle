4339b792fc058fa6c06a99f2c5ca072b4b11b2f4
smallbrownbike <ekinseydev@gmail.com>
2017-12-02 10:46:11 -0800

    more styles connect db

--- imports/ui/Components/Chat.js
+++ imports/ui/Components/Chat.js
@@ -1,60 +1,72 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Scroll from 'react-scroll';
+import { withTracker } from 'meteor/react-meteor-data';
+const scroll = Scroll.animateScroll;
 
-export default class Chat extends React.Component{
+import { Chats } from '../../api/chats'
+
+class ViewBox extends React.Component{
+	componentDidUpdate(){
+		if(this.lastMessage){
+			scroll
+			ReactDOM.findDOMNode(this.lastMessage).scrollIntoView()
+		}
+	}
+
+	render(){
+		return(
+			<div className='viewBox'>
+				{this.props.chats.map(chat => {
+					return (
+						<div ref={ref => this.lastMessage = ref} className='message'>
+							<div className='username'>{chat.username}</div>
+							<div className='date'>{new Date(chat.date).toLocaleString()}</div>							
+							<div className='text'>{chat.text}</div>
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
+	
+}
+
+class Chat extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {value: ''}
+	}
+	handleChange = e => {
+		this.setState({value: e.target.value})
+	}
+	handleKeyPress = e => {
+		if(e.key === 'Enter'){
+			Chats.insert({
+				username: 'Farts',
+				text: this.state.value,
+				date: Date.now()
+			})
+			this.setState({value: ''})
+		}
+	}
 	render(){
 		return (
 			<div className='container'>
 				<div className='online'></div>
 				<div className='chatBox'>
-				<div className='viewBox'>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
+					<ViewBox chats={this.props.chats}/>
+					<div>
+						<input placeholder='Message' className='chatInput' value={this.state.value} onKeyPress={this.handleKeyPress} onChange={this.handleChange} />
 					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-					<div className='message'>
-						<div className='username'>Username</div>
-						<div className='text'>Text</div>
-					</div>
-				</div>
-				<input placeholder='Message' className='chatInput' />
 				</div>
 			</div>
 		)
 	}
 }
+
+export default withTracker(() => {
+	return {
+	  chats: Chats.find({}, {sort: {date: 1}}).fetch(),
+	};
+})(Chat);
