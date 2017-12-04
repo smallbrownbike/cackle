99c4781c83e738aa46cf259118eee3059c6f1218
smallbrownbike <ekinseydev@gmail.com>
2017-12-03 22:49:55 -0800

    require username to create new message

--- imports/ui/Components/Chat.js
+++ imports/ui/Components/Chat.js
@@ -135,7 +135,7 @@ class Chat extends React.Component{
 		this.setState({value: e.target.value})
 	}
 	handleKeyPress = e => {
-		if(this.state.value && e.key === 'Enter'){
+		if(this.state.value && username && e.key === 'Enter'){
 			const id = this.props.chats[0]._id,
 						message = {
 							username: username,
