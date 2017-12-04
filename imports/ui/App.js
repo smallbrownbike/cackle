44126bde9574f3fb4be960f8fb4e88c3dc7c0af1
smallbrownbike <ekinseydev@gmail.com>
2017-12-03 22:21:05 -0800

    style changes

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,8 +8,7 @@ export default class App extends React.Component{
 			<div>
 				
 				<div style={{position: 'absolute', top: 0, left: 0, marginLeft: '2em'}}>
-					<h1 style={{marginBottom: '0em'}}>CACKLE</h1>
-					<h5 style={{marginTop: '0em'}}>dynamic chatroom generator</h5>
+					<h1 style={{fontSize: '2.2em', color: '#383838', borderBottom: '.5px solid #00000066', display: 'inline-block'}}>cackle</h1>
 				</div>
 				<Chat />
 			</div>
